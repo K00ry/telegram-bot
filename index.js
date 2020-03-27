d109dc2136059676695ece59a358c8a5a4fe10f4
Kourosh Mirzaei <kouroshmirzaei@Kouroshs-MBP.fios-router.home>
2020-03-27 17:05:44 -0400

    ready for lower case

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const path = require('path');
 const app = express();
 const axios = require('axios');
-
-var bodyParser = require('body-parser');
+const Telegraf = require('telegraf');
+const AnyCase = require('telegraf-anycase-commands');
+const bot = new Telegraf('1129108256:AAFzOZOQRIpLTXmXTjodD5bPcrN2VxvcG0k');
+const commandArgsMiddleware = require('./commandArgs');
 app.use(express.static(path.join(__dirname,'public')));
+
+
+bot.use(AnyCase.lowercase());
+// var bodyParser = require('body-parser');
+
 // app.use(bodyParser.json());// for parsing application/json
 //
 // app.use(
@@ -12,45 +19,79 @@ app.use(express.static(path.join(__dirname,'public')));
 //         extended: true
 //     })
 // );
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
 
-app.get("/users", (req, res) => {
-    res.json([
-        { name: "William", location: "Abu Dhabi" },
-        { name: "Chris", location: "Vegas" }
-    ]);
+// app.get("/users", (req, res) => {
+//     res.json([
+//         { name: "William", location: "Abu Dhabi" },
+//         { name: "Chris", location: "Vegas" }
+//     ]);
+// });
+//
+// app.post("/user", (req, res) => {
+//     const { name, location } = req.body;
+//
+//     res.send({ status: "User created", name, location });
+// });
+
+bot.use(  (ctx,next) =>{
+    ctx.update.message.text.toLowerCase();
+
+    next();
 });
 
-app.post("/user", (req, res) => {
-    const { name, location } = req.body;
 
-    res.send({ status: "User created", name, location });
+bot.hears('Jaber', ctx => {
+    console.log(ctx.update.message.text); // command object
 });
-// app.use('/public/img', express.static(__dirname + '/public/img'));
-
-const Telegraf = require('telegraf');
-
-const bot = new Telegraf('1129108256:AAFzOZOQRIpLTXmXTjodD5bPcrN2VxvcG0k');
+// bot.command('lights', ctx => ctx.reply('Hello from the bot side.'));
 bot.start((ctx) => ctx.reply('Sallam Doostan man Nadere kooni hastam! mitoonid soalate zir ro az man beporsid :' +
     "\n"+
-    'nader key miyay?' +
+    'Nader key miyay?' +
     "\n"+
-    'nader che joori koon midi?' +
+    'Nader che joori koon midi?' +
     "\n"+
-    'nader bere biyad chand dar miyad?' +
+    'Nader bere biyad chand dar miyad?' +
     "\n"+
-    'nader kiramo mikhori ya mibary?'));
+    'Nader kiramo mikhori ya mibary?'));
 bot.help((ctx) => ctx.reply('Koon mikhay behet bedam?'));
 // bot.on('sticker', (ctx) => ctx.reply('👍'));
 // bot.on('sticker', (ctx) => ctx.replyWithPhoto({
 //     url: 'https://picsum.photos/200/300/?random',
 //     filename: 'kitten.jpg'
 // }));
-bot.hears('nader key miyay?',(ctx) => ctx.reply('tamrin kon!! tamriin kon miyam!!!'));
-bot.hears('nader che joori koon midi?',(ctx) => ctx.reply('aval az koonet mikhoram!! badan miparam roosh!!! badan kireto mikonam toosh!! '));
-bot.hears('nader bere biyad chand dar miyad?',(ctx) => ctx.reply('eeeeeeennnaaaa!!'));
-bot.hears('nader kiramo mikhori ya mibary?',(ctx) => ctx.reply('oskole vamoonde kireto mikhoram ye poolam behet miidam!!!'));
+//  ()=>{
+//     let string = ctx;
+//     ctx.toLowerCase();
+// };
+bot.use(commandArgsMiddleware());
+
+
+bot.hears('Nader key miyay?',(ctx) => ctx.reply('tamrin kon!! tamriin kon miyam!!!'));
+bot.hears('Nader che joori koon midi?',(ctx) => ctx.reply('aval az koonet mikhoram!! badan miparam roosh!!! badan kireto mikonam toosh!! '));
+bot.hears('Nader bere biyad chand dar miyad?',(ctx) => ctx.reply('eeeeeeennnaaaa!!'));
+bot.hears('Nader kiramo mikhori ya mibary?',(ctx) => ctx.reply('oskole vamoonde kireto mikhoram ye poolam behet miidam!!!'));
+bot.hears('Nader',(ctx) => ctx.reply('haa! chi mikhay???!!' ));
+bot.hears('kir',(ctx) => ctx.reply('Mikhoram😍😍😍!!!!' ));
+// bot.hears('😂',(ctx) => ctx.reply('نِمَک!!!' ));
+// bot.hears('😂',(ctx) => ctx.replyWithPhoto({
+//     url: 'https://telegram-bot-eight.now.sh/nemak.jpg',
+//     filename: 'nemak.jpg'
+// }));
+
+// bot.hears('🤣',(ctx) => ctx.replyWithPhoto({
+//     url: 'https://telegram-bot-eight.now.sh/nader-shaki.jpg',
+//     filename: 'nader-shaki.jpg'
+// }));
+// bot.hears('😁',(ctx) => ctx.replyWithPhoto({
+//     url: 'https://telegram-bot-eight.now.sh/nader.png',
+//     filename: 'nader.png'
+// }));
+bot.hears('😒',(ctx) => ctx.reply('Khafesho Mehran!!!' ));
+
+
+
 
 bot.mention('@KooneNaderBot', (ctx) => ctx.reply('Koon mikhay behet bedam?'));
 
